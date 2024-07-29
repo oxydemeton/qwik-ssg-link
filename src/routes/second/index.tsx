9bce07ac1286614efbb8e53020aa7d1f02f6f6fe
M B <mablasteam@gmail.com>
2024-07-29 10:00:29 +0200

    added pages

--- src/routes/second/index.tsx
+++ src/routes/second/index.tsx
@@ -1,5 +1,5 @@
 import { component$ } from "@builder.io/qwik";
-import { Link, type DocumentHead } from "@builder.io/qwik-city";
+import { Link } from "@builder.io/qwik-city";
 import { isServer } from "@builder.io/qwik/build";
 
 export default component$(() => {
@@ -7,24 +7,14 @@ export default component$(() => {
     <>
       <h1>Hi 👋</h1>
       <div>
-        root/first page
+        Second page
         <br />
         This page {isServer? "server": "client"} rendered
         <br />
         Happy coding.
         <br />
-        <Link href="/second">To Second Page</Link>
+        <Link href="/">To root/first Page</Link>
       </div>
     </>
   );
 });
-
-export const head: DocumentHead = {
-  title: "Welcome to Qwik",
-  meta: [
-    {
-      name: "description",
-      content: "Qwik site description",
-    },
-  ],
-};
